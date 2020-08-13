439170d4b361a26aac667ed69c95dc5b7c1f08a8
jgarcia15121982 <johfregarcia@gmail.com>
2020-08-12 20:34:12 -0500

    [ADD] Setup MySQL Test

---
+++ setup_mysql_test.sql
@@ -0,0 +1,9 @@
+-- Script that prepares a MySQL server for the project
+-- AirBnB clone
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost';
+SET PASSWORD FOR 'hbnb_test'@'localhost' = 'hbnb_test_pwd';
+USE hbnb_test_db;
+GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+FLUSH PRIVILEGES;
