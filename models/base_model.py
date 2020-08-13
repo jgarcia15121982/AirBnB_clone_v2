1c9931936a18cb3f7ebfd126891657a85003d5a4
Juan Jose <1561@holbertonschool.com>
2020-08-13 00:40:22 +0000

    [add] importations

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+import sqlalchemy
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
 
+Base = declarative_base()
 
 class BaseModel:
     """A base class for all hbnb models"""
