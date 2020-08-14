cf77c092b349198c5c016823963d8a37c7c2e05a
Juan Jose <1561@holbertonschool.com>
2020-08-14 01:06:04 +0000

    [added] import

--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 import models
+from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
