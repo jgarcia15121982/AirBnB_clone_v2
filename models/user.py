3e4958e1f45df18b8f983807e1fbc9bae4ae7c46
Juan Jose <1561@holbertonschool.com>
2020-08-14 01:27:54 +0000

    [fix] pep8

--- models/user.py
+++ models/user.py
@@ -5,6 +5,7 @@ from sqlalchemy import Column, String
 import models
 from sqlalchemy.orm import relationship
 
+
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
