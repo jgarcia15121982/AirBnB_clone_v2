2cd6e3f6ab3f1ee8209581106653a01afb139ee0
Juan Jose <1561@holbertonschool.com>
2020-08-17 22:46:24 +0000

    [fix] synatx 2

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,7 +2,7 @@
 """ Generates a .tgz archive """
 
 
-from fabric.api import *
+from fabric.api import local
 from datetime import datetime
 import os
 
@@ -11,10 +11,10 @@ def do_pack():
     """ generate a tgz file """
     time_f = '%Y%m%d%H%M%S'
     try:
-        if os.path.versions not exists:
+        if not os.path.exists('versions'):
             local('mkdir versions')
-            to = 'version/web_static_{}.tgz'
-            .format(datetime.now().strftime(time_f))
+            to = 'version/web_static_{}.tgz'.format
+            (datetime.now().strftime(time_f))
 
             local('tar -cvzf {} web_static'.format(to))
             return(to)
