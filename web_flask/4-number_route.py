0be82b068a1e66b4bc2ccf80bb9d1f7b38134ee2
Juan Jose <1561@holbertonschool.com>
2020-09-01 21:38:58 +0000

    [fix & add] convertr type

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -31,7 +31,7 @@ def python(text="is cool"):
     return 'Python ' + text.replace("_", " ")
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
     """ if n is an integer return a message """
     pr = print("{:d} is a number".format(n))
