0a5b0d9499ebd2f7f382b4c0125bbfd6d9c9fcdf
Juan Jose <1561@holbertonschool.com>
2020-09-01 18:09:30 +0000

    [add] first version

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Will start a flask web application """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ will display a message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ will display hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ display C with the value of text """
+    return 'C' + text.replace("_", " ")
+
+
+if __name__ == "__main__":
+    """ function of the main """
+    app.run(host='0.0.0.0', port=5000)
