e48b1737ef73466d1c71aac437b457006b67e26d
Juan Jose <1561@holbertonschool.com>
2020-09-01 22:31:10 +0000

    [add] integer validation

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -37,7 +37,7 @@ def number(n):
     return ("{:d} is a number".format(n))
 
 
-@app.route('/number_template/<n>', strict_slashes=False)
+@app.route('/number_template/<int:n>', strict_slashes=False)
 def html(n):
     """ display a html if n is an integer """
     return render_template('5-number.html', value=n)
