04806fe355762c3a5140c345d4fcfcfecd2773dd
Juan Jose <1561@holbertonschool.com>
2020-09-01 21:27:29 +0000

    [add] first version

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Will start a flask web application """
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ will display a message """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ will display hbnb """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_is_fun(text):
+    """ display C with the value of text """
+    return 'C ' + text.replace("_", " ")
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text="is cool"):
+    """ display python with the value of text """
+    return 'Python ' + text.replace("_", " ")
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    """ if n is an integer return a message """
+    i = isinstance(n, int)
+    pr = print("{} is a number".format(n))
+    if n == i:
+        return (pr)
+    else:
+        pass
+
+
+if __name__ == "__main__":
+    """ function of the main """
+    app.run(host='0.0.0.0', port=5000)
