c6bfb3378958e8c40c9616d3ba4d8216f8ad3835
Juan Jose <1561@holbertonschool.com>
2020-09-02 16:43:55 +0000

    [fix]

--- models/state.py
+++ models/state.py
@@ -2,26 +2,29 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 import sqlalchemy
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Column, String
 import models
+from os import environ
 from models.city import City
+
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete, delete-orphan",
-                          backref='state')
 
-    @property
-    def cities(self):
-        """method that return the cities"""
-
-        dict_2 = models.storage.all(City)
-        list_2 = []
-        for res1 in dict_2.values():
-            if res1.state_id == self.id:
-                list_2.append(res1)
-        return list_2
+    if environ.get('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City", cascade="all, delete, delete-orphan",
+                              backref='state')
+    else:
+        @property
+        def cities(self):
+            """method that return the cities"""
+            dict_2 = models.storage.all(City)
+            list_2 = []
+            for res1 in dict_2.values():
+                if res1.state_id == self.id:
+                    list_2.append(res1)
+            return list_2
